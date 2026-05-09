ac37321469f11a400ee88aa8d07c6ac3c700f2c6
adanainv3-creator <adanainv3@gmail.com>
2026-05-09 21:34:00 +0300

    Force push via env variable: 2026-05-09 21:34:00

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -91,3 +91,39 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.3")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.3")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.3")
+
+    implementation("androidx.activity:activity-compose:1.9.0")
+
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
+
+    val bom = platform("androidx.compose:compose-bom:2024.06.00")
+
+    implementation(bom)
+
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material-icons-extended")
+
+    implementation("androidx.compose.animation:animation")
+    implementation("androidx.compose.foundation:foundation")
+
+    debugImplementation("androidx.compose.ui:ui-tooling")
+
+    implementation("androidx.savedstate:savedstate-ktx:1.2.1")
+
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.google.code.gson:gson:2.11.0")
+
+    implementation("org.iq80.snappy:snappy:0.4")
+
+    testImplementation("junit:junit:4.13.2")
+
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+}
+
+configurations.all {
+    resolutionStrategy.cacheChangingModulesFor(0, TimeUnit.SECONDS)
+}
