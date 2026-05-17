85cb714be99b5e58cf533a557e47a929c15a4734
adanainv3-creator <adanainv3@gmail.com>
2026-05-17 13:48:36 +0300

    Force push via env variable: 2026-05-17 13:48:36

---
+++ app/src/main/kotlin/com/oxclient/auth/DeviceCodeLoginActivity.kt
@@ -0,0 +1,12 @@
+// auth/DeviceCodeLoginActivity.kt
+package com.oxclient.auth
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class DeviceCodeLoginActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        finish() // şimdilik boş bırak
+    }
+}
