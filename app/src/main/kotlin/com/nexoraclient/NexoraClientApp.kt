f55a30ad38181a7b24ebfe2444ccc1564e578f4f
adanainv3-creator <adanainv3@gmail.com>
2026-07-27 23:38:49 +0300

    Force push via env variable: 2026-07-27 23:38:49

--- app/src/main/kotlin/com/nexoraclient/NexoraClientApp.kt
+++ app/src/main/kotlin/com/nexoraclient/NexoraClientApp.kt
@@ -23,7 +23,6 @@ import com.nexoraclient.module.combat.KillAura
 import com.nexoraclient.module.combat.KillAuraPro
 import com.nexoraclient.module.combat.PcAura
 import com.nexoraclient.module.misc.ChatSpammer
-import com.nexoraclient.module.misc.AntiLag
 import com.nexoraclient.module.misc.ChatAdvertiser
 import com.nexoraclient.module.misc.Disconnect
 import com.nexoraclient.module.movement.AntiKnockback
@@ -135,7 +134,6 @@ class NexoraClientApp : Application() {
             FOVChanger(),
             ArrayListModule(),
             AutoArmor(),
-            AntiLag(),
             TPAuraPC(),
             AutoScaffold(),  // AutoRegear kaldırıldı, AutoScaffold eklendi
             ChatSpammer(),
